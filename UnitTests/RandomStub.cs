18906de175a77debb4f1ba4f031997dc1f5ba688
Wojciech Toman <tomanw@gmail.com>
2019-09-09 11:37:19 +0200

    Added RandomStub class to be able to test methods that use random number generation.

---
+++ UnitTests/RandomStub.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    class RandomStub : Random
+    {
+        public int NextDoubleValue { get; set; }
+        public int NextIntValue { get; set; }
+
+        public override double NextDouble()
+        {
+            return this.NextDoubleValue;
+        }
+
+        public override int Next(int maxValue)
+        {
+            return this.NextIntValue;
+        }
+
+        public override int Next()
+        {
+            return this.NextIntValue;
+        }
+
+        public override int Next(int minValue, int maxValue)
+        {
+            return this.NextIntValue;
+        }
+    }
+}
