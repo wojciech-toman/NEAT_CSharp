a907a19b065b0f055149c7eef6b232f6622f471c
Wojciech Toman <tomanw@gmail.com>
2019-09-23 19:28:35 +0200

    Added T.Clamp(T min, T max) extension method which clamps the value to min, max.

---
+++ NeuralEvolution/HelperMethods.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NEAT_CSharp
+{
+    public static class HelperMethods
+    {
+        public static T Clamp<T>(this T value, T inclusiveMin, T inclusiveMax) where T : IComparable<T> {
+            if (value.CompareTo(inclusiveMin) < 0) return inclusiveMin;
+            if (value.CompareTo(inclusiveMax) > 0) return inclusiveMax;
+
+            return value;
+        }
+    }
+}
---
+++ UnitTests/HelperMethodsTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NEAT_CSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NEAT_CSharp.Tests
+{
+    [TestClass()]
+    public class HelperMethodsTests
+    {
+        [DataTestMethod()]
+        [DataRow(1, 5, 1, 1)]
+        [DataRow(1, 5, 5, 5)]
+        [DataRow(1, 5, 0, 1)]
+        [DataRow(1, 5, 6, 5)]
+        [DataRow(1, 5, 2, 2)]
+        public void ClampTest_TestInteger(int min, int max, int value, int expected)
+        {
+            int retVal = value.Clamp(min, max);
+            Assert.AreEqual(expected, retVal);
+        }
+
+        [DataTestMethod()]
+        [DataRow(1, 5, 1, 1)]
+        [DataRow(1, 5, 5, 5)]
+        [DataRow(1, 5, 0, 1)]
+        [DataRow(1, 5, 6, 5)]
+        [DataRow(1, 5, 2, 2)]
+        public void ClampTest_TestFloat(float min, float max, float value, float expected)
+        {
+            float retVal = value.Clamp(min, max);
+            Assert.AreEqual(expected, retVal);
+        }
+    }
+}
